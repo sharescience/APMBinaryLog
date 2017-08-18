5cd1410d2fdb3434b9ef726f05c9abc86f7b2fbe
SuWeipeng <weipeng.su@outlook.com>
2017-08-18 09:59:38 +0800

    加入状态机值的倍数times，方便与其它数据一起同屏分析

--- FSMAnalysis.m
+++ FSMAnalysis.m
@@ -18,11 +18,12 @@ plot_count = 0;
 color = rand(size(FSM_label, 1), 3);
 except = []; % fill except ids here
 axis_x = 1;  % 1-n as X axis 2-TimeUS as X axis
+times  = 1;  % FSM value times
 for i = 1:size(FSM_label, 1)
     if ~isempty(find(FSM(i,1)==except))
         continue
     end
-    plot(h, eval(sprintf('%s%d%s%d','state(',axis_x,').id',FSM(i,1))), eval(sprintf('%s%d','state(3).id',FSM(i,1))),'Color',color(i,:),'Marker','o','MarkerFaceColor',color(i,:),'LineStyle','--');
+    plot(h, eval(sprintf('%s%d%s%d','state(',axis_x,').id',FSM(i,1))), eval(sprintf('%s%d','state(3).id',FSM(i,1)))*times,'Color',color(i,:),'Marker','o','MarkerFaceColor',color(i,:),'LineStyle','--');
     plot_count = plot_count + 1;
     if(isequal(plot_count,1))
         leg = strcat('''','id',sprintf('%d',FSM(i,1)),'''');
