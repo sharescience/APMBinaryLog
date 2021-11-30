28394eccb7448ada9c4f2428caa2f8222c3d9ae8
SuWeipeng <weipeng.su@outlook.com>
2021-11-30 23:22:01 +0800

    不输出 debug 过程

--- APMBinaryLog.m
+++ APMBinaryLog.m
@@ -56,7 +56,7 @@ function APMBinaryLog
                 end                
                 FIELDS   = regexp(char(labels(i,1)), ',|''', 'split'); 
                 avalible = not(cellfun(@isempty,FIELDS));
-                FIELDS = FIELDS(avalible)
+                FIELDS = FIELDS(avalible);
                 C = C(:,2:size(C,2));
                 eval(strcat(lower(char(group_name{i})),'=cell2struct(C,FIELDS,2);'));
                 eval(char(strcat('assignin(','''','base','''',', ','''',char(lower(char(group_name{i}))),'''',',', char(lower(char(group_name{i}))), ');')));
